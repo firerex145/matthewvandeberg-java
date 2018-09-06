bd3eb05e2f2fcd178341a3121da47eed86cd9039
firerex145 <firerex145@gmail.com>
2018-09-06 08:57:40 -0700

    Added Pikachu project (from school)

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,9 @@
+public class Pikachu {
+    public static void main(String[] args) {
+
+        System.out.println("  (\\__/)");
+        System.out.println("  (*^.^)");
+        System.out.println("z(_(\")(\")");
+
+    }
+}
