a489dc42e5825617aeeabee188de6ad443e0185c
firerex145 <firerex145@gmail.com>
2018-09-17 09:20:30 -0700

    Start of challenge

---
+++ ChallengeOneAndTwo/src/ChallengeOne.java
@@ -0,0 +1,29 @@
+public class ChallengeOne {
+    public static void main(String[] args){
+        tenTimesTen();
+    }
+    public static void ten(){
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+    }
+    public static void tenTimesTen(){
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+        ten();
+    }
+}
