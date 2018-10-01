a75c1bb024906b94da3e898bc1d46025e945b0ed
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2018-10-01 07:20:58 -0700

    Added the first bit of the rocket

--- DrawRocket/src/DrawRocket.java
+++ DrawRocket/src/DrawRocket.java
@@ -6,11 +6,74 @@ Period 1
 Project #6 - Draw Rocket
  */
 public class DrawRocket {
-    public static final int SIZE = 3;
-    public static void main(String[] args){
 
+    public static final int SIZE = 3;
+
+    public static void main(String[] args){
+        drawCone();
+        drawDivider();
+        drawUp();
+        drawDown();
     }
-    public static void drawCones(){
-        for (i = 0;)
+    public static void drawCone(){
+        for (int i = 1; i < (SIZE * 2); i++){
+            for(int j = 0; j < i; j++){
+                System.out.print("/");
+            }
+            System.out.print("**");
+            for(int g = 0; g < i; g++){
+                System.out.print("\\");
+            }
+            System.out.println();
+        }
+    }
+    public static void drawDivider() {
+        System.out.print("+");
+        for (int i = 0; i < (SIZE * 2); i++) {
+            System.out.print("=*");
+        }
+        System.out.println("+");
+    }
+    public static void drawUp(){
+        for (int k = 1; k < (SIZE + 1); k++) {
+            System.out.print("|");
+            for (int i = 0; i < (SIZE - k); i++) {
+                System.out.print(".");
+            }
+            for (int j = 0; j < k; j++) {
+                System.out.print("/\\");
+            }
+            for (int l = 0; l < (SIZE - k); l++) {
+                System.out.print("..");
+            }
+            for (int m = 0; m < k; m++) {
+                System.out.print("/\\");
+            }
+            for (int n = 0; n < (SIZE - k); n++) {
+                System.out.print(".");
+            }
+            System.out.println("|");
+        }
+    }
+    public static void drawDown(){
+        for (int k = 1; k < (SIZE + 1); k++) {
+            System.out.print("|");
+            for (int i = 0; i < (SIZE - k); i++) {
+                System.out.print(".");
+            }
+            for (int j = 0; j < k; j++) {
+                System.out.print("/\\");
+            }
+            for (int l = 0; l < (SIZE - k); l++) {
+                System.out.print("..");
+            }
+            for (int m = 0; m < k; m++) {
+                System.out.print("/\\");
+            }
+            for (int n = 0; n < (SIZE - k); n++) {
+                System.out.print(".");
+            }
+            System.out.println("|");
+        }
     }
 }
