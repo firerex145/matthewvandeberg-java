8fd11b6fad4a17a0c47152bc24e666774559d805
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2018-09-30 23:24:02 -0700

    Added DrawRocket project plus started on it
    From home

---
+++ DrawRocket/src/DrawRocket.java
@@ -0,0 +1,16 @@
+/*
+Matthew Vandeberg
+AP CS A
+Period 1
+9-27-18
+Project #6 - Draw Rocket
+ */
+public class DrawRocket {
+    public static final int SIZE = 3;
+    public static void main(String[] args){
+
+    }
+    public static void drawCones(){
+        for (i = 0;)
+    }
+}
