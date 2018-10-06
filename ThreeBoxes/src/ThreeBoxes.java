e95434fd3a2ee46b7d956d549dae0bfcc250b64a
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2018-10-06 13:51:19 -0700

    Final version of ThreeBoxes project
    only added documentation and header

--- ThreeBoxes/src/ThreeBoxes.java
+++ ThreeBoxes/src/ThreeBoxes.java
@@ -1,7 +1,14 @@
+/*
+Matthew Vandeberg
+AP CS A
+Period 1
+10-5-18
+Project #5 - ThreeBoxes
+ */
 public class ThreeBoxes {
 
-    public static final int ROWS = 3;
-    public static final int COLUMNS = 6;
+    public static final int ROWS = 3; //class constant for number of rows in each box
+    public static final int COLUMNS = 6; //class constant for number of columns in each box
 
     public static void main (String[] args){
         star();
@@ -11,25 +18,25 @@ public class ThreeBoxes {
         pounds();
     }
 
-    public static void star() {
-        for (int i = 0; i < ROWS; i++){
-            for (int j = 0; j < COLUMNS; j++){
+    public static void star() { //prints box of stars
+        for (int i = 0; i < ROWS; i++){ //determines amount of rows in box of stars
+            for (int j = 0; j < COLUMNS; j++){ //determines amount of columns in box of stars
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
-    public static void dollars() {
-        for (int i = 0; i < ROWS; i++){
-            for (int j = 0; j < COLUMNS; j++){
+    public static void dollars() { //prints box of dollar signs
+        for (int i = 0; i < ROWS; i++){ //determines amount of rows in box of dollar signs
+            for (int j = 0; j < COLUMNS; j++){ //determines amount of columns in box of dollar signs
                 System.out.print("$ ");
             }
             System.out.println();
         }
     }
-    public static void pounds() {
-        for (int i = 0; i < ROWS; i++){
-            for (int j = 0; j < COLUMNS; j++){
+    public static void pounds() { //prints box of pound symbols
+        for (int i = 0; i < ROWS; i++){ //determines amount of rows in box of pound symbols
+            for (int j = 0; j < COLUMNS; j++){ //determines amount of columns in box of pound symbols
                 System.out.print("# ");
             }
             System.out.println();
