81543e7060a7d519d231c54b0cfb6ee4e9519c34
firerex145 <firerex145@gmail.com>
2018-10-12 09:07:35 -0700

    added parameters to ThreeBoxes project (from school)

--- ThreeBoxes/src/ThreeBoxes.java
+++ ThreeBoxes/src/ThreeBoxes.java
@@ -1,38 +1,18 @@
 public class ThreeBoxes {
 
-    public static final int ROWS = 3;
-    public static final int COLUMNS = 6;
-
     public static void main (String[] args){
-        star();
-        System.out.println();
-        dollars();
-        System.out.println();
-        pounds();
+        printBox(5, 3, '*');
+        printBox(7, 8, '$');
+        printBox(2, 2, '#');
     }
 
-    public static void star() {
-        for (int i = 0; i < ROWS; i++){
-            for (int j = 0; j < COLUMNS; j++){
-                System.out.print("* ");
+    public static void printBox(int height, int width, char print) {
+        for (int i = 0; i < height; i++){
+            for (int j = 0; j < width; j++){
+                System.out.print(print);
             }
             System.out.println();
         }
-    }
-    public static void dollars() {
-        for (int i = 0; i < ROWS; i++){
-            for (int j = 0; j < COLUMNS; j++){
-                System.out.print("$ ");
-            }
-            System.out.println();
-        }
-    }
-    public static void pounds() {
-        for (int i = 0; i < ROWS; i++){
-            for (int j = 0; j < COLUMNS; j++){
-                System.out.print("# ");
-            }
-            System.out.println();
-        }
+        System.out.println();
     }
 }
