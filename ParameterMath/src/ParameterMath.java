df47bea3dfabcc16690f7b385370166331754379
firerex145 <firerex145@gmail.com>
2018-10-22 08:49:59 -0700

    Final version. Has an add, minimum, and square root method

--- ParameterMath/src/ParameterMath.java
+++ ParameterMath/src/ParameterMath.java
@@ -1,2 +1,16 @@
 public class ParameterMath {
+    public static void main(String[] args) {
+        System.out.println(add(2, 4));
+        System.out.println(minimum(3, 6, 1));
+        System.out.println(squareRoot(3, 4, 13));
+    }
+    public static int add(int a, int b) {
+        return (a + b);
+    }
+    public static int minimum(int a, int b, int c) {
+        return(Math.min(a, Math.min(b, c)));
+    }
+    public static double squareRoot(int a, int b, int c) {
+        return(Math.sqrt((Math.min(a, Math.min(b, c)) + Math.max(a, Math.max(b, c)))));
+    }
 }
