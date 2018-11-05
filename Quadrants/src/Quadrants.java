ecea084dd89a0335aa73d560db272c0527b07897
firerex145 <firerex145@gmail.com>
2018-11-05 08:40:35 -0800

    Made it better (added a line before the printout and added a return for point on origin).

--- Quadrants/src/Quadrants.java
+++ Quadrants/src/Quadrants.java
@@ -1,10 +1,15 @@
 public class Quadrants {
     public static void main(String[] args){
-        System.out.println(quadrant(6, -9));
+        System.out.println();
+        System.out.println(quadrant(0, 0));
     }
     public static String quadrant(double x, double y){
         if (x == 0.0 || y == 0.0){
-            return ("The point is on an axis.");
+            if (x == 0 && y == 0){
+                return("The point is on the origin.");
+            } else {
+                return ("The point is on an axis.");
+            }
         } else if (x > 0.0){
             if (y > 0.0){
                 return("The point is in quadrant 1.");
