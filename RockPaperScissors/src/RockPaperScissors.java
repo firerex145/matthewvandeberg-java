d5c82b838588f548daa38c698f6a8fb22b56f570
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2018-11-08 21:27:19 -0800

    Final version of RockPaperScissors project
    without comments

--- RockPaperScissors/src/RockPaperScissors.java
+++ RockPaperScissors/src/RockPaperScissors.java
@@ -1,31 +1,67 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
     static Scanner player = new Scanner(System.in);
     static String name;
     public static void main(String[] args){
-        int humanWins = 0;
-        int compWins = 0;
-        int ties = 0;
         int humanGames = 0;
         int compGames = 0;
         int playAgain = 0;
         int games = 1;
+        Random rand = new Random();
+        String rematch;
         System.out.println("What is your name?");
         name = player.next();
         while(playAgain == 0) {
+            int humanWins = 0;
+            int compWins = 0;
+            int ties = 0;
+            int rematchPrompt = 0;
             System.out.println("Would you like to play three or five rounds?");
             int rounds = player.nextInt();
             while (!(rounds == 3 || rounds == 5)) {
                 System.out.println("You can only play three or five rounds. Renter how many rounds you want to play.");
                 rounds = player.nextInt();
             }
-            while((rounds + ties) > 0){
+            while(rounds > 0 - ties){
                 System.out.println("Enter 0 (rock), 1 (paper), or 2 (scissors).");
                 int input = player.nextInt();
-                System.out.println("Rock, Paper, Scissors; Shoot!");
-                int compInput = (int)(3 * Math.random());
+                int compInput = rand.nextInt(3);
                 String roundResults = play(input, compInput);
+                System.out.println(roundResults + "\n");
+                if (roundResults.endsWith("wins") == true) {
+                    humanWins++;
+                } else if (roundResults.endsWith("loses") == true) {
+                    compWins++;
+                } else {
+                    ties++;
+                }
+                System.out.print("Match Standings: ");
+                System.out.printf(name + " wins -- %2s  Computer wins -- %2s  Ties -- %2s\n\n", humanWins, compWins, ties);
+                rounds--;
+            }
+            if (humanWins > compWins) {
+                System.out.println("game " + games + " results: " + name + " beats computer " + humanWins + " to " + compWins);
+                humanGames++;
+            } else {
+                System.out.println("game " + games + " results: computer beats " + name + " " + compWins + " to " + humanWins);
+                compGames++;
+            }
+            System.out.printf("Overall standings: " + name + " -- %2s wins Computer -- %2s wins\n", humanGames, compGames);
+            System.out.println("Rematch? (yes/no)");
+            rematch = player.next();
+            while (rematchPrompt == 0) {
+                if (rematch.equalsIgnoreCase("yes")) {
+                    games++;
+                    rematchPrompt++;
+                } else if (rematch.equalsIgnoreCase("no")) {
+                    playAgain = 1;
+                    rematchPrompt++;
+                } else {
+                    System.out.println("Please answer with yes or no");
+                    rematch = player.next();
+                }
             }
         }
     }
@@ -43,9 +79,9 @@ public class RockPaperScissors {
         } else {
             human = "scissors";
         }
-        if (playerInput == 0){
+        if (compInput == 0){
             computer = "rock";
-        } else if (playerInput == 1){
+        } else if (compInput == 1){
             computer = "paper";
         } else {
             computer = "scissors";
@@ -53,14 +89,15 @@ public class RockPaperScissors {
         return(results(human, computer));
     }
     public static String results(String humanChoice, String compChoice){
+        System.out.println("Rock, Paper, Scissors; Shoot!\n");
         System.out.printf(name + " chooses: %10s\n", humanChoice);
         System.out.printf("Computer chooses: %10s\n", compChoice);
         if (humanChoice.equals("paper") && compChoice.equals("rock") || humanChoice.equals("rock") && compChoice.equals("scissors") || humanChoice.equals("scissors") && compChoice.equals("paper")){
             return(humanChoice + " beats " + compChoice + " - " + name + " wins");
-        } else if(){
-
+        } else if(compChoice.equals("paper") && humanChoice.equals("rock") || compChoice.equals("rock") && humanChoice.equals("scissors") || compChoice.equals("scissors") && humanChoice.equals("paper")){
+            return(compChoice + " beats " + humanChoice + " - " + name + " loses");
         } else {
-
+            return("Match results in a tie");
         }
     }
 }
