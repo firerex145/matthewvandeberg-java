b6eb34b3162e4fa1fd4bee0ffae05d392172058f
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2018-11-26 21:17:29 -0800

    Final version of both worksheet files

--- FourPointOneWorksheet/src/FourPointOneWorksheet.java
+++ FourPointOneWorksheet/src/FourPointOneWorksheet.java
@@ -1,60 +1,117 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FourPointOneWorksheet {
     public static void main (String[] args){
         //Exercise 1
-        int[] list1 = new int[8];
-
-        //Exercise 2a
-        list1[0] = 1;
-        list1[1] = 2;
-        list1[2] = 3;
-        list1[3] = 4;
-        list1[4] = 5;
-        list1[5] = 6;
-        list1[6] = 7;
-        list1[7] = 8;
-
-        //Exercise 2b
-        int[] list2b = {2, 5, 3, 7, 8};
-
-        //Exercise 2c
-        int[] list2c = {1, 2, 3, 4, 5};
-
-        //Exercise 2d
-        Scanner input = new Scanner(System.in);
-        int[] list2d = new int[4];
-        for (int i : list2d) {
-            System.out.println("Input a number.");
-            list2d[i] = input.nextInt();
-            System.out.println();
-        }
-
-        //Exercise 2e
-        int[] list2e = {-4, 19, 3, 20, 72, 58};
-
-        //Exercise 3a
-        System.out.println(list2d[list2d.length - 1]);
-
-        //Exercise 3b
-        System.out.println(list2d[list2d.length / 2 - 1]);
-
-        //Exercise 3c
-        for (int j : list2d){
-            list2d[j]--;
-        }
+//        int[] list1 = new int[8];
+//
+//        //Exercise 2a
+//        list1[0] = 1;
+//        list1[1] = 2;
+//        list1[2] = 3;
+//        list1[3] = 4;
+//        list1[4] = 5;
+//        list1[5] = 6;
+//        list1[6] = 7;
+//        list1[7] = 8;
+//
+//        //Exercise 2b
+//        int[] list2b = {2, 5, 3, 7, 8};
+//
+//        //Exercise 2c
+//        int[] list2c = {1, 2, 3, 4, 5};
+//
+//        //Exercise 2d
+//        Scanner input = new Scanner(System.in);
+//        int[] list2d = new int[4];
+//        for (int i : list2d) {
+//            System.out.println("Input a number.");
+//            list2d[i] = input.nextInt();
+//            System.out.println();
+//        }
+//
+//        //Exercise 2e
+//        int[] list2e = {-4, 19, 3, 20, 72, 58};
+//
+//        //Exercise 3a
+//        System.out.println(list2d[list2d.length - 1]);
+//
+//        //Exercise 3b
+//        System.out.println(list2d[list2d.length / 2 - 1]);
+//
+//        //Exercise 3c
+//        for (int j : list2d){
+//            list2d[j]--;
+//        }
 
         //Exercise 3d
-        int[] even = new int[500];
-        for (int n : even){
-            if ((n % 2) == 0){
-                even[n / 2] = n;
-            }
+//        int[] even = new int[500];
+//        for (int n = 0; n < even.length * 2; n++){
+//            if ((n % 2) == 0){
+//                even[n / 2] = n;
+//            }
+//        }
+//
+//
+//        //Exercise 3f
+//        System.out.println(even[100]);
+//
+//        //Exercise 3g
+//        System.out.println(even[278] + " " + even[456]);
+
+        //Exercise 3h
+        int[] odds = new int[Math.abs(-4 - 5) / 2 + 2];
+        odds[0] = -4;
+        for(int m = 2; m <= odds.length; m++){
+            odds[m - 1] = (m - 3) * 2 - 1;
         }
+        System.out.println(Arrays.toString(odds));
 
-        //Exercise 3f
-        System.out.println(even[100]);
-
-        //Exercise3g
+        //Exercise 3i
+        int num1 = 0;
+        int num2 = 1;
+        int min = Math.min(num1, num2);
+        int max = Math.max(num1, num2);
+        int[] modOdds = new int[Math.abs(max - min) / 2];
+        if (min == -1 && max == 1){
+            modOdds = Arrays.copyOf(modOdds, modOdds.length + 1);
+        }
+        if(min % 2 == 0 || max % 2 == 0){
+            modOdds = Arrays.copyOf(modOdds, modOdds.length + 2);
+            if(min % 2 == 0 && max % 2 == 0){
+                modOdds[0] = min;
+                int j = 1;
+                for(int i = min + 1; i <= max; i += 2){
+                    modOdds[j] = i;
+                    j++;
+                }
+                modOdds[modOdds.length - 1] = max;
+            } else {
+                if(min % 2 == 0){
+                    modOdds[0] = min;
+                    int j = 1;
+                    for(int i = min + 1; i <= max; i += 2){
+                        modOdds[j] = i;
+                        j++;
+                    }
+                } else {
+                    int j = 0;
+                    for(int i = min; i <= max; i += 2){
+                        modOdds[j] = i;
+                        j++;
+                    }
+                    modOdds[modOdds.length - 1] = max;
+                }
+            }
+        } else {
+            int j = 0;
+            for(int i = min; i < max; i += 2){
+                modOdds[j] = i;
+                j++;
+            }
+            modOdds[modOdds.length - 1] = max;
+        }
+        System.out.println(Arrays.toString(modOdds));
     }
 }
---
+++ FourPointOneWorksheet/src/FourPointTwoWorksheet.java
@@ -0,0 +1,98 @@
+import java.util.Arrays;
+
+public class FourPointTwoWorksheet {
+    public static void main(String[] args){
+        int[] array1 = {10, -2, 4, -4, 9, -5, 19, -7, 39, -1};
+        int[] numbers = {11, 4, 99, 2, 44, 0, 0, 0};
+
+        //Exercise 1d
+        max(array1);
+
+        //Exercise 1e
+        average(array1);
+
+        //Exercise 2a
+        Arrays.copyOf(numbers, numbers.length * 2);
+
+        //Exercise 2b
+        Arrays.copyOfRange(numbers, 0, 3);
+
+        //Exercise 2c
+        Arrays.fill(numbers, 42);
+
+        //Exercise 3
+        range(array1);
+
+        //Exercise 4
+        mode(array1);
+    }
+
+    //Exercise 1d
+    public static int max(int[] maxArray){
+        int maxInt = 0;
+        for (int i : maxArray){
+            if (i > maxInt){
+                maxInt = i;
+            }
+        }
+        return maxInt;
+    }
+
+    //Exercise 1e
+    public static double average(int[] averageArray){
+        double sum = 0.0;
+        for (int i : averageArray){
+            sum += i;
+        }
+        double averageVal = sum / averageArray.length;
+        return averageVal;
+    }
+
+    //Exercise 3
+    public static int range(int[] rangeArray){
+        int max = 0;
+        for(int j : rangeArray){
+            if(j > max){
+                max = j;
+            }
+        }
+        int min = max;
+        for(int i : rangeArray){
+            if(i < min){
+                min = i;
+            }
+        }
+        int rangeVal = max - min + 1;
+        return rangeVal;
+    }
+
+    //Exercise 4
+    public static int mode(int[] modeArray){
+        Arrays.sort(modeArray);
+        int[] singleElement = {modeArray[modeArray.length - 1], 0};
+        int modeVal;
+        int element;
+        int count;
+        for(int i : modeArray) {
+            count = 0;
+            element = i;
+            for (int j : modeArray) {
+                if(element == j){
+                    count++;
+                }
+            }
+            if(count >= singleElement[1]){
+                if(count == singleElement[1]){
+                    if(element < singleElement[0]){
+                        singleElement[0] = element;
+                    }
+                }else {
+                    singleElement[0] = element;
+                }
+                singleElement[1] = count;
+            }
+        }
+        modeVal = singleElement[0];
+        return modeVal;
+    }
+}
