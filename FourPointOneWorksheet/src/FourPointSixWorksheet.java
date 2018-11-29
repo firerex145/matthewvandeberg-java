5c99298f45522831557927655e8e999720494950
firerex145 <firerex145@gmail.com>
2018-11-29 09:18:59 -0800

    Finished to Exercise 12

--- FourPointOneWorksheet/src/FourPointSixWorksheet.java
+++ FourPointOneWorksheet/src/FourPointSixWorksheet.java
@@ -30,19 +30,40 @@ public class FourPointSixWorksheet {
         studentSeating[1][3] = teacherName.next();
 
         //Exercise 8
-        String temp = "";
         String target = "";
         for(int i = 0; i < studentSeating.length; i++){
             for(int j = 2; j < studentSeating.length; j++){
                 if(studentSeating[i][j - 2].equals("Matthew Vandeberg")){
-                    temp = studentSeating[i][j - 2];
                     studentSeating[i][j - 2] = target;
                 }
                 if(studentSeating[i][j - 2].equals(target)){
-                    studentSeating[i][j - 2] = temp;
+                    studentSeating[i][j - 2] = "Matthew Vandeberg";
 
                 }
             }
+        }
+
+        //Exercise 9
+        int[][] temperature = new int[10][2];
+
+        //Exercise 10
+        String[][] chessBoard = new String[8][8];
+        Arrays.fill(chessBoard[1], "pawn");
+        Arrays.fill(chessBoard[6], "pawn");
+
+        //Exercise 11
+        int[][] data = new int[4][7];
+        for (int i = 0; i < data[2].length; i++){
+            data[2][i] = i + 1;
+        }
+
+        //Exercise 12
+        int temp1;
+        int[][] matrix = new int[6][8];
+        for(int i = 0; i < matrix[1].length; i++){
+            temp1 = matrix[1][i];
+            matrix[1][i] = matrix[4][i];
+            matrix[4][i] = temp1;
         }
     }
 }
