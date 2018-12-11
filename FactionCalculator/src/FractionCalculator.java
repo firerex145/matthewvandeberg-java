2cd06100baae36a181802e8f283b0c9d5a45a9b9
firerex145 <firerex145@gmail.com>
2018-12-11 09:20:27 -0800

    Initial commit v2 (from school)

--- FactionCalculator/src/FractionCalculator.java
+++ FactionCalculator/src/FractionCalculator.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class FractionCalculator {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int loop = 0;
+        while (loop == 0){
+            System.out.print("Enter something: ");
+
+            for (int i = 0; i < 3; i++){
+                if (input.nextLine().contains("quit")){
+                    System.out.println("Program ended");
+                    loop++;
+                    break;
+                } else {
+                    System.out.println(input.next());
+                }
+            }
+            System.out.println("");
+        }
+    }
 }
