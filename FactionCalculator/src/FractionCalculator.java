57f7cd61031b458f541e93cbc99fc3a81bb5468c
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2018-12-23 22:56:38 -0800

    Can now do basic simplifications and can handle more negative numbers
    Also discovered and fixed a bug when calculating whole numbers (negative and non-negative) and when dividing plus the "negative mixed number" bug.

--- FactionCalculator/src/FractionCalculator.java
+++ FactionCalculator/src/FractionCalculator.java
@@ -16,31 +16,53 @@ public class FractionCalculator {
                 equationArray = parse(something);
             }
 
-            int firstNume;
-            int firstDenom;
-            if (equationArray[3].equals("+") || equationArray[3].equals("-") || equationArray[3].equals("*") || equationArray[3].equals("")) {
-                firstNume = (Integer.parseInt(equationArray[2]) * Integer.parseInt(equationArray[0]) + Integer.parseInt(equationArray[1]));
-                firstDenom = Integer.parseInt(equationArray[2]);
-                equationArray[0] = "0";
-            } else {
-                firstNume = Integer.parseInt(equationArray[0]);
-                if (equationArray[1].equals("+") || equationArray[1].equals("-") || equationArray[1].equals("*") || equationArray[1].equals("")) {
-                    firstDenom = 1;
-                } else {
-                    firstDenom = Integer.parseInt(equationArray[1]);
-                }
-            }
             String operator = "";
             int operatorIndex = 0;
+            int divide = 0;
             for (int i = 1; i < 4; i++) {
                 if (equationArray[i].equals("+") || equationArray[i].equals("-") || equationArray[i].equals("*") || equationArray[i].equals("")) {
                     operator = equationArray[i];
                     operatorIndex = i;
+                    if (equationArray[i].equals("")) {
+                        equationArray[i] = "/";
+                        equationArray[i + 1] = equationArray[i + 2];
+                        equationArray[i + 2] = equationArray[equationArray.length - 1];
+                        divide++;
+                    }
+                    i = 4;
                 }
+            }
+            int firstNume;
+            int firstDenom;
+            if (equationArray[1].equals("+") || equationArray[1].equals("-") || equationArray[1].equals("*") || equationArray[1].equals("/")) {
+                firstDenom = 1;
+                firstNume = Integer.parseInt(equationArray[0]);
+            } else if (equationArray[3].equals("+") || equationArray[3].equals("-") || equationArray[3].equals("*") || equationArray[3].equals("/")) {
+                if (Integer.parseInt(equationArray[0]) < 0) {
+                    firstNume = (Integer.parseInt(equationArray[2]) * Integer.parseInt(equationArray[0]) - Integer.parseInt(equationArray[1]));
+                } else {
+                    firstNume = (Integer.parseInt(equationArray[2]) * Integer.parseInt(equationArray[0]) + Integer.parseInt(equationArray[1]));
+                }
+                firstDenom = Integer.parseInt(equationArray[2]);
+                equationArray[0] = "0";
+            } else {
+                firstNume = Integer.parseInt(equationArray[0]);
+                firstDenom = Integer.parseInt(equationArray[1]);
             }
             int secondNume;
             int secondDenom;
-            if ((operatorIndex + 1) == (equationArray.length - 1)) {
+            if (divide == 1) {
+                if ((operatorIndex + 1) == (equationArray.length - 2)) {
+                    secondNume = Integer.parseInt(equationArray[operatorIndex + 1]);
+                    secondDenom = 1;
+                } else if ((operatorIndex + 1) == (equationArray.length - 3)) {
+                    secondNume = Integer.parseInt(equationArray[operatorIndex + 1]);
+                    secondDenom = Integer.parseInt(equationArray[equationArray.length - 2]);
+                } else {
+                    secondNume = Integer.parseInt(equationArray[equationArray.length - 2]) * Integer.parseInt(equationArray[operatorIndex + 1]) + Integer.parseInt(equationArray[operatorIndex + 2]);
+                    secondDenom = Integer.parseInt(equationArray[equationArray.length - 2]);
+                }
+            } else if ((operatorIndex + 1) == (equationArray.length - 1)) {
                 secondNume = Integer.parseInt(equationArray[operatorIndex + 1]);
                 secondDenom = 1;
             } else if ((operatorIndex + 1) == (equationArray.length - 2)) {
@@ -50,6 +72,7 @@ public class FractionCalculator {
                 secondNume = Integer.parseInt(equationArray[equationArray.length - 1]) * Integer.parseInt(equationArray[operatorIndex + 1]) + Integer.parseInt(equationArray[operatorIndex + 2]);
                 secondDenom = Integer.parseInt(equationArray[equationArray.length - 1]);
             }
+            System.out.println(firstNume + "/" + firstDenom + " " + secondNume + "/" + secondDenom);
             if (operator.equals("+")) {
                 System.out.println(add(firstNume, firstDenom, secondNume, secondDenom));
             } else if (operator.equals("-")) {
@@ -63,41 +86,91 @@ public class FractionCalculator {
     }
 
     public static String add(int firstNume, int firstDenom, int secondNume, int secondDenom) {
-        int Numerator;
-        int Denominator;
+        int numerator;
+        int denominator;
         if (firstDenom == secondDenom) {
-            Numerator = firstNume + secondNume;
-            Denominator = firstDenom;
+            numerator = firstNume + secondNume;
+            denominator = firstDenom;
         } else {
-            Numerator = (firstNume * secondDenom) + (secondNume * firstDenom);
-            Denominator = firstDenom * secondDenom;
+            numerator = (firstNume * secondDenom) + (secondNume * firstDenom);
+            denominator = firstDenom * secondDenom;
         }
-        return Numerator + "/" + Denominator;
+        if (denominator < 0) {
+            denominator = Math.abs(denominator);
+            numerator = -numerator;
+        }
+        if (numerator < 0 && denominator < 0) {
+            numerator = Math.abs(numerator);
+            denominator = Math.abs(denominator);
+        }
+        if (numerator == denominator) {
+            return "1";
+        }
+        return numerator + "/" + denominator;
     }
 
     public static String subtract(int firstNume, int firstDenom, int secondNume, int secondDenom) {
-        int Numerator;
-        int Denominator;
+        int numerator;
+        int denominator;
         if (firstDenom == secondDenom) {
-            Numerator = firstNume - secondNume;
-            Denominator = firstDenom;
+            numerator = firstNume - secondNume;
+            denominator = firstDenom;
         } else {
-            Numerator = (firstNume * secondDenom) - (secondNume * firstDenom);
-            Denominator = firstDenom * secondDenom;
+            numerator = (firstNume * secondDenom) - (secondNume * firstDenom);
+            denominator = firstDenom * secondDenom;
         }
-        return Numerator + "/" + Denominator;
+        if (denominator < 0) {
+            denominator = Math.abs(denominator);
+            numerator = -numerator;
+        }
+        if (numerator < 0 && denominator < 0) {
+            numerator = Math.abs(numerator);
+            denominator = Math.abs(denominator);
+        }
+        if (numerator == denominator) {
+            return "1";
+        }
+        return numerator + "/" + denominator;
     }
 
     public static String multiply(int firstNume, int firstDenom, int secondNume, int secondDenom) {
-        int Numerator = firstNume * secondNume;
-        int Denominator = firstDenom * secondDenom;
-        return Numerator + "/" + Denominator;
+        int numerator = firstNume * secondNume;
+        int denominator = firstDenom * secondDenom;
+        if (denominator < 0) {
+            denominator = Math.abs(denominator);
+            numerator = -numerator;
+        }
+        if (numerator < 0 && denominator < 0) {
+            numerator = Math.abs(numerator);
+            denominator = Math.abs(denominator);
+        }
+        if (denominator == 1) {
+            return numerator + "";
+        }
+        if (numerator == denominator) {
+            return "1";
+        }
+        return numerator + "/" + denominator;
     }
 
     public static String divide(int firstNume, int firstDenom, int secondNume, int secondDenom) {
-        int Numerator = firstNume * secondDenom;
-        int Denominator = firstDenom * secondNume;
-        return Numerator + "/" + Denominator;
+        int numerator = firstNume * secondDenom;
+        int denominator = firstDenom * secondNume;
+        if (denominator < 0) {
+            denominator = Math.abs(denominator);
+            numerator = -numerator;
+        }
+        if (numerator < 0 && denominator < 0) {
+            numerator = Math.abs(numerator);
+            denominator = Math.abs(denominator);
+        }
+        if (denominator == 1) {
+            return numerator + "";
+        }
+        if (numerator == denominator) {
+            return "1";
+        }
+        return numerator + "/" + denominator;
     }
 
     public static String[] parse(String equation) {
