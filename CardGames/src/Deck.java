00a7af14dba1102a18050b9044ae8cfdd3b49513
firerex145 <firerex145@gmail.com>
2019-03-04 09:14:47 -0800

    Imported Card Tester and added a client class (from school)

--- CardGames/src/Deck.java
+++ CardGames/src/Deck.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 
 public class Deck {
-    private ArrayList<Card> deck;
+    private ArrayList<Card> deck = new ArrayList<>();
     private int size;
     public Deck(String[] rank, String[] suit, int[] value) {
-        for (int i = 0; i < suit.length * rank.length; i++) {
-            deck.add(new Card(rank[i], suit[i], value[i]));
+        for (int i = 0; i < rank.length; i++) {
+            for(int j = 0; j < suit.length; j++) {
+                deck.add(new Card(rank[i], suit[j], value[i]));
+            }
         }
         //shuffle();
     }
@@ -19,5 +21,12 @@ public class Deck {
     public String deal() {
         size -= 1;
         return deck.get(size).toString();
+    }
+    public String toString() {
+        String temp = deck.get(0).toString();
+        for (int i = 1; i < deck.size(); i ++) {
+            temp += ", " + deck.get(i).toString();
+        }
+        return temp;
     }
 }
